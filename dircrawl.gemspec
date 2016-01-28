54ee32d83670bbe4e3995878d5b93531c1f4a062
shidash <shidash@shidash.com>
2016-01-28 12:59:46 -0500

    First commit

---
+++ dircrawl.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'dircrawl'
+  s.version     = '0.0.1'
+  s.date        = '2016-01-28'
+  s.summary     = 'Run block on all files in dir'
+  s.description = 'Run block on all files in dir'
+  s.authors     = ['M. C. McGrath']
+  s.email       = 'shidash@shidash.com'
+  s.files       = ["lib/dircrawl.rb"]
+  s.homepage    =
+    'https://github.com/TransparencyToolkit/dircrawl'
+  s.license       = 'GPL'
+end
---
+++ lib/dircrawl.rb
@@ -0,0 +1,56 @@
+require 'json'
+require 'pry'
+
+class DirCrawl
+  def initialize(path, output_dir, ignore_includes, process_block, include_block, *args)
+    @path = path
+    @output_dir = output_dir
+    @ignore_includes = ignore_includes
+    include_block.call
+    @process_block = process_block
+    @output = Array.new
+    crawl_dir(path, *args)
+  end
+
+  # Figure out where to write it
+  def get_write_dir(dir, file)
+    dir_save = dir.gsub(@path, @output_dir)
+    return dir_save+"/"+file
+  end
+
+  # Create if they don't exist
+  def create_write_dirs(dir)
+    dirs = dir.split("/")
+    dirs.delete("")
+    overallpath = ""
+    dirs.each do |d|
+      Dir.mkdir(overallpath+"/"+d) if !File.directory?(overallpath+"/"+d)
+      overallpath += ("/"+d)
+    end
+  end
+  
+  # Crawl dir and call block for each file
+  def crawl_dir(dir, *args)
+    Dir.foreach(dir) do |file|
+      next if file == '.' or file == '..'
+      # Go to next dir
+      if File.directory?(dir+"/"+file)
+        crawl_dir(dir+"/"+file, *args)
+
+      # Process file
+      elsif !file.include?(@ignore_includes)
+        processed = @process_block.call(dir+"/"+file, *args)
+        @output.push(processed)
+
+        # Write to file
+        create_write_dirs(dir.gsub(@path, @output_dir))
+        File.write(get_write_dir(dir, file), JSON.pretty_generate(processed))
+      end
+    end
+  end
+
+  # Get the output array
+  def get_output
+    return JSON.pretty_generate(@output)
+  end
+end
